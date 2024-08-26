4250c7597403952e2be141d2166b1d14b4c7a4b8
InesKouki <ines.kouki@esprit.tn>
2024-08-26 21:22:08 +0200

    adding frontend pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://4.208.24.1:8090/api/v1' // Development API URL
+    baseUrl: 'http://4.207.39.200:8090/api/v1' // Development API URL
   };
   
