59781fb4182569b7d4ade61d312c07173189cfdd
InesKouki <ines.kouki@esprit.tn>
2024-08-27 21:58:37 +0200

    testing ingress

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://4.207.39.200:8090/api/v1' // Development API URL
+    baseUrl: 'http://connector.northeurope.aksapp.io/api/v1' // Development API URL
   };
   
